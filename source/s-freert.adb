e0feff3a22ec082c9efa81997dfcb4aaed0114c5
Vadim Godunko <vgodunko@gmail.com>
2026-05-04 10:42:00 +0400

    Map Ada priorities to FreeRTOS priorities directly.

--- source/s-freert.adb
+++ source/s-freert.adb
@@ -8,10 +8,14 @@ package body System.FreeRTOS is
    function To_FreeRTOS_Priority
      (Priority : System.Priority) return UBaseType_t is
    begin
-      return
-        UBaseType_t
-          (((Priority - System.Any_Priority'First) * configMAX_PRIORITIES)
-             / (System.Priority'Last - System.Priority'First + 1));
+      --  FreeRTOS priorities are mapped directly to Ada priorities. Assert
+      --  that the Ada priorities are starts at zero, and that the number of
+      --  Ada priorities matches configured number of FreeRTOS priorities.
+      pragma Assert (System.Priority'First = 0);
+      pragma Assert
+        (System.Priority'Last + 1 = System.FreeRTOS.configMAX_PRIORITIES);
+
+      return UBaseType_t (Priority);
    end To_FreeRTOS_Priority;
 
 end System.FreeRTOS;
